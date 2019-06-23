5fa4973225909d07f465125e3e3dccb4b7d818cc
amsoft2017 <32521319+amsoft2017@users.noreply.github.com>
2019-06-23 19:58:37 +0500

    урок 28 end

---
+++ app/Repositories/CoreRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class CoreRepository
+ * @package App\Repositories
+ *
+ * Репозиторий работы с сущностью
+ * Может выдаввать наборы данных
+ * Не может создавать\изменять наборы данных
+ */
+abstract class CoreRepository
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * CoreRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->model = app($this->getModelClass());
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function getModelClass();
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|Model|mixed
+     */
+    protected function startConditions(){
+        return clone $this->model;
+    }
+}
